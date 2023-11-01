43861ef5a05254749f2634364e9e3db3cef03221
ivanguerron21 <guerronivan@gmail.com>
2023-11-01 14:07:57 -0500

    upgraded model and created api

--- challenge/api.py
+++ challenge/api.py
@@ -1,6 +1,19 @@
 import fastapi
+from fastapi import Request, HTTPException
+from challenge.model import DelayModel
+import os
+import pandas as pd
 
 app = fastapi.FastAPI()
+model = DelayModel()
+saved_model = 'saved_model' 
+if not os.path.exists(saved_model):
+    os.mkdir(saved_model)
+if len(os.listdir(saved_model)) == 0:
+    data = pd.read_csv(filepath_or_buffer="data/data.csv", low_memory=False)
+    features, target = model.preprocess(data=data, target_column="delay")
+    model.fit(features=features, target=target)
+
 
 @app.get("/health", status_code=200)
 async def get_health() -> dict:
@@ -9,5 +22,14 @@ async def get_health() -> dict:
     }
 
 @app.post("/predict", status_code=200)
-async def post_predict() -> dict:
-    return
+async def post_predict(request: Request) -> dict:
+    try:
+        data = await request.json()
+        data = data['flights']
+        data = pd.DataFrame(data)
+        features = model.preprocess(data=data)
+        preds = model.predict(features)
+        pred = {'predict': preds}
+        return pred
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
--- challenge/model.py
+++ challenge/model.py
@@ -1,16 +1,11 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from xgboost import XGBClassifier
 from sklearn.exceptions import NotFittedError
-from sklearn.linear_model import LogisticRegression
-import seaborn as sns
-import warnings
+import os
 from typing import Tuple, Union, List
-from sklearn.model_selection import train_test_split
-from sklearn.utils import shuffle
 from datetime import datetime
-from sklearn.metrics import confusion_matrix, classification_report
+import json
 
 class DelayModel:
 
@@ -18,8 +13,7 @@ class DelayModel:
         self
     ):
         self._model = XGBClassifier(random_state=1, learning_rate=0.01, scale_pos_weight = 4.44) # Model should be saved in this attribute.
-        self.default_target = None
-        self.default_features = None
+        self.save_model_path = 'saved_model'
 
     def preprocess(
         self,
@@ -57,23 +51,37 @@ class DelayModel:
                 return 1
             else:
                 return 0
+            
         def get_min_diff(data):
             fecha_o = datetime.strptime(data['Fecha-O'], '%Y-%m-%d %H:%M:%S')
             fecha_i = datetime.strptime(data['Fecha-I'], '%Y-%m-%d %H:%M:%S')
             min_diff = ((fecha_o - fecha_i).total_seconds())/60
             return min_diff
-
-        data['high_season'] = data['Fecha-I'].apply(is_high_season)
-        data['min_diff'] = data.apply(get_min_diff, axis = 1)
-        threshold_in_minutes = 15
-        data['delay'] = np.where(data['min_diff'] > threshold_in_minutes, 1, 0)
-        # training_data = shuffle(data[['OPERA', 'MES', 'TIPOVUELO', 'SIGLADES', 'DIANOM', 'delay']], random_state = 111)
+        
+        def check_features(feat: pd.DataFrame) -> bool:
+            dict_opera = open('challenge/opera.json')
+            dict_opera = json.load(dict_opera)
+            opera_list = dict_opera['OPERA']
+            for col in feat.columns:
+                if col.__contains__('TIPOVUELO'):
+                    if col.replace('TIPOVUELO_', '') not in ['N', 'I']:
+                        return False
+                elif col.__contains__('OPERA'):
+                    if col.replace('OPERA_', '') not in opera_list:
+                        return False
+                elif col.__contains__('MES'):
+                    month = int(col.replace('MES_', ''))
+                    if month > 12 or month < 0:
+                        return False
+            return True
+                    
         features = pd.concat([
             pd.get_dummies(data['OPERA'], prefix = 'OPERA'),
             pd.get_dummies(data['TIPOVUELO'], prefix = 'TIPOVUELO'), 
             pd.get_dummies(data['MES'], prefix = 'MES')], 
             axis = 1
         )
+
         top_10_features = [
             "OPERA_Latin American Wings", 
             "MES_7",
@@ -86,16 +94,22 @@ class DelayModel:
             "OPERA_Sky Airline",
             "OPERA_Copa Air"
         ]
-        features = features[top_10_features]
-        self.default_features = features
+
         if target_column == None:
-            target_column = 'delay'
-            target = data[[target_column]]
-            self.default_target = target
+            check = check_features(features)
+
+            if not check:
+                raise Exception('Non-existent feature in input data')
+            
+            features = features.reindex(top_10_features, axis=1, fill_value=0)
             return features
         else:
+            features = features.reindex(top_10_features, axis=1, fill_value=0)
+            data['high_season'] = data['Fecha-I'].apply(is_high_season)
+            data['min_diff'] = data.apply(get_min_diff, axis = 1)
+            threshold_in_minutes = 15
+            data['delay'] = np.where(data['min_diff'] > threshold_in_minutes, 1, 0)
             target = data[[target_column]]
-            self.default_target = target
             return features, target
 
     def fit(
@@ -111,6 +125,10 @@ class DelayModel:
             target (pd.DataFrame): target.
         """
         self._model.fit(features, target)
+        if self.save_model_path:
+            if not os.path.exists(self.save_model_path):
+                os.mkdir(self.save_model_path)
+            self._model.save_model(self.save_model_path + '/model.json')
 
     def predict(
         self,
@@ -126,7 +144,8 @@ class DelayModel:
             (List[int]): predicted targets.
         """
         try:
-            return self._model.predict(features).tolist()
+            preds = self._model.predict(features).tolist()
+            return preds
         except NotFittedError:
-            self.fit(self.default_features, self.default_target)
+            self._model.load_model(self.save_model_path + '/model.json')
             return self._model.predict(features).tolist()
